d1a449f9f22aff590755fb8484b60d9ea8acfcbc
Thanh Sang <anhsang31797@example.com>
2024-04-19 14:15:44 +0700

    active song

--- main.js
+++ main.js
@@ -81,9 +81,9 @@ const app = {
     },
   ],
   render: function () {
-    const htmls = this.songs.map((song) => {
+    const htmls = this.songs.map((song,index) => {
       return `
-            <div class="song">
+            <div class="song ${index === this.cureentIndex ? 'active' : ''}">
             <div class="thumb" style="background-image: url('${song.image}')">
             </div>
             <div class="body">
@@ -178,6 +178,7 @@ const app = {
         _this.nextSong();
       }
       audio.play();
+      _this.render();
     };
     // khi prev song
     prevBtn.onclick = function () {
@@ -187,6 +188,7 @@ const app = {
         _this.prevSong();
       }
       audio.play();
+      _this.render();
     };
 
     // xử lý bật / tắt random
